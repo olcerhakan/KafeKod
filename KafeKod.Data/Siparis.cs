8f7aca2fbdea81dc04292a761342b02d356d6ffa
Hakan Ölçer <olcerhakan1@gmail.com>
2020-03-11 18:33:22 +0300

    Data'ya EF entegre edildi.

--- KafeKod.Data/Siparis.cs
+++ KafeKod.Data/Siparis.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,42 +8,25 @@ using System.Threading.Tasks;
 namespace KafeKod.Data
 {
     public enum SiparisDurum { Aktif, Odendi, Iptal }
+
+    [Table("Siparisler")]
     public class Siparis
     {
-        public Siparis()
-        {
-            SiparisDetaylar = new List<SiparisDetay>();
-        }
+        
+        public int Id { get; set; }
 
         public int MasaNo { get; set; }
+
         public DateTime? AcilisZamani { get; set; }
+
         public DateTime? KapanisZamani { get; set; }
+
         public SiparisDurum Durum { get; set; }
-        public List<SiparisDetay> SiparisDetaylar { get; set; } = new List<SiparisDetay>();
-        // public string ToplamTutarTL => string.Format("{0:0.00}₺", ToplamTutar());  // bu proptur.  { get {...}}
 
         public decimal OdenenTutar { get; set; }
 
-        public string ToplamTutarTL
-        {
-            get
-            {
-                return string.Format("{0:0.00}₺", ToplamTutar());
-            }
-        }
-        public decimal ToplamTutar()
-        {
-            return SiparisDetaylar.Sum(x => x.Tutar());
-            /*
-           decimal toplam = 0;
-           foreach (var item in SiparisDetaylar)
-           {
-               toplam +=item.Tutar();
-           }
-           return toplam;
-       */
-        }
-
-
+        public virtual List<SiparisDetay> SiparisDetaylar { get; set; }
+       
+      
     }
 }
--- KafeKod.Data/Urun.cs
+++ KafeKod.Data/Urun.cs
@@ -1,27 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KafeKod.Data
 {
-    public class Urun: IComparable
+    [Table("Urunler")]
+    public class Urun 
     {
+        public int Id { get; set; }
+        
+        [Required,MaxLength(50)]
         public string UrunAd { get; set; }
+
         public decimal BirimFiyat { get; set; }
 
-        public int CompareTo(object obj)
-        {
-            return UrunAd.CompareTo(((Urun)obj).UrunAd);
-        }
+        //bir ürün birden çok sipariş listesinde olabilir
+        public virtual List<SiparisDetay> SiparisDetaylar { get; set; }
+
 
         public override string ToString()
         {
             return string.Format("{0} - {1:0.00} ₺", UrunAd, BirimFiyat);
         }
-
-
 
     }
 }
